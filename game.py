897af548282f8b73291841e369291e9e4ddf843e
Terence Grover <terencegrover@gmail.com>
2026-01-19 19:17:38 +0100

    pose tweaks

--- game.py
+++ game.py
@@ -543,7 +543,7 @@ def main():
     pose = mp_pose.Pose(
         static_image_mode=False,
         model_complexity=1,
-        smooth_landmarks=True,
+        smooth_landmarks=False,
         enable_segmentation=False,
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5,
